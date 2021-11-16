562b312de4ab8e5aa69f5f4a59326b90e3201f69
Tony <tonyreed1542@gmail.com>
2021-11-15 20:09:09 -0600

    init

---
+++ src/classes/Drink.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Food;
+
+/*
+ * Base class that will hold various information about our drink menu
+ */
+abstract class Drink {
+    // TODO flesh out
+}
---
+++ src/classes/Food.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Food;
+
+/*
+ * Base class that will hold various information about our menu
+ */
+abstract class Food
+{
+    // TODO flesh out
+}
