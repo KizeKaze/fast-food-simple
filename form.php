d05d89571bff32564e0ad903e3a2b49889a4b1f0
Tony <tonyreed1542@gmail.com>
2021-11-15 20:45:57 -0600

    basiiiiiiic css

--- form.php
+++ form.php
@@ -1,23 +1,34 @@
-<html>
+<html lang="en">
+<head>
+    <title>Fast Food</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-    <form action="submit" method="post">
-        <div>
-            <label for="name">Name</label>
-            <input id="name" name="name" type="text" />
-        </div>
-    
-        <div>
-            <label for="description">Description</label>
-            <input id="description" name="description" type="text" />
-        </div>
-    
-        <div>
-            <label for="cost">Cost</label>
-            <input id="cost" name="cost" type="text" />
-        </div>
-        
-        <br>
-        <button>Submit</button>
-    </form>
+    <div class="container">
+        <h3>Add an item to the menu</h3>
+        <form action="submit" method="post">
+            <div>
+                <div>
+                    <label for="name">Name</label>
+                </div>
+                <div>
+                    <input id="name" name="name" type="text" />
+                </div>
+            </div>
+
+            <div>
+                <label for="description">Description</label>
+                <input id="description" name="description" type="text" />
+            </div>
+
+            <div>
+                <label for="cost">Cost</label>
+                <input id="cost" name="cost" type="text" />
+            </div>
+
+            <br>
+            <button>Submit</button>
+        </form>
+    </div>
 </body>
 </html>
