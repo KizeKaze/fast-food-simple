aa33cb2a83ddba3da459c0ea78e8335c5c33a0d5
kizekaze <rae337@hotmail.com>
2022-02-26 10:10:56 -0600

    fixed css on menu_list.php, will take another pass at css in issue #18

--- menu_list.php
+++ menu_list.php
@@ -2,37 +2,59 @@
 <?php include "includes/nav.php" ?>
 
 
-
-<div class="container">
-    <div class="search-container">
-        <form action="menu_list.php" method="post">
-            <select name="type">
-                <option value="0">All</option>
-                <?php $results = $menu->getType() ?>
-                <?php foreach ($results as $row) {
-                    echo "<option value=" . $row['type_id'] . ">" . $row['type'] . "</option>";
-
-                } ?>
-
-            </select>
-            <input type="text" name="search" placeholder="Search...">
-            <input type="submit" name="submit">
-        </form>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <form action="menu_list.php" method="post">
+                    <select name="type">
+                        <option value="0">All</option>
+                        <?php $results = $menu->getType() ?>
+                        <?php foreach ($results as $row) {
+                            echo "<option value=" . $row['type_id'] . ">" . $row['type'] . "</option>";
+                        } ?>
+                    </select>
+                    <input type="text" name="search" placeholder="Search...">
+                    <input type="submit" name="submit">
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-<div class="container">
-    <div>
 
-        <?php
-        if (isset($_POST['submit']) || isset($_POST['type'])) {
-            $type = trim(htmlspecialchars($_POST['type']));
-            $search = trim(htmlspecialchars($_POST['search']));
-            $result = $menu->getSearch($search, $type);
-            if (!($result)) {
-                echo "No search result found";
+    <div class="container">
+            <?php
+            if (isset($_POST['submit']) || isset($_POST['type'])) {
+                $type = trim(htmlspecialchars($_POST['type']));
+                $search = trim(htmlspecialchars($_POST['search']));
+                $result = $menu->getSearch($search, $type);
+                if (!($result)) {
+                    echo "No search result found";
+                } else {
+                    foreach ($result as $row) {
+                        echo "<div class='card'>";
+                        echo "<div class='card-body'>";
+
+                        $id = $row['id'];
+                        $name = $row['name'];
+                        $description = $row['description'];
+                        $cost = $row['cost'];
+                        $type = $row['type'];
+                        ?>
+                        <label>ID: <?= $id ?></label><br>
+                        <label>Name: <?= $name ?></label><br>
+                        <label>Description: <?= $description ?></label><br>
+                        <label>Cost: <?= $cost ?></label><br>
+                        <label>Type: <?= $type ?></label>
+                        <?php
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                }
             } else {
+                $result = $menu->getItems();
+
                 foreach ($result as $row) {
-                    echo "<div class='menu_result_div'>";
+                    echo "<div class='card'>";
+                    echo "<div class='card-body'>";
 
                     $id = $row['id'];
                     $name = $row['name'];
@@ -44,37 +66,16 @@
                     <label>Name: <?= $name ?></label><br>
                     <label>Description: <?= $description ?></label><br>
                     <label>Cost: <?= $cost ?></label><br>
-                    <label>Type: <?= $type ?></label>
+                    <label>Type: <?= $type ?></label><br>
+                    <form action="edit_menu_item.php" method="get">
+                        <button type="submit" class="btn btn-primary" name="edit" value=<?= $id ?>>Edit</button>
+                    </form>
                     <?php
+                    echo "</div>";
                     echo "</div>";
                 }
             }
-        } else {
-            $result = $menu->getItems();
 
-            foreach ($result as $row) {
-                echo "<div class='menu_result_div'>";
-
-                $id = $row['id'];
-                $name = $row['name'];
-                $description = $row['description'];
-                $cost = $row['cost'];
-                $type = $row['type'];
-                ?>
-                <label>ID: <?= $id ?></label><br>
-                <label>Name: <?= $name ?></label><br>
-                <label>Description: <?= $description ?></label><br>
-                <label>Cost: <?= $cost ?></label><br>
-                <label>Type: <?= $type ?></label><br>
-                <form action="edit_menu_item.php" method="get">
-                    <button type="submit" class="btn btn-primary" name="edit" value=<?=$id?>>Edit</button>
-                </form>
-                <?php
-                echo "</div>";
-            }
-        }
-
-        ?>
-    </div>
-</div>
+            ?>
+        </div>
 <?php include "includes/footer.php"; ?>
