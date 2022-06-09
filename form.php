c4e4d5088c95426b3a951ff04e09a9ed308a70ee
kizekaze <rae337@hotmail.com>
2022-06-08 19:43:33 -0500

    adjusted success display to match website layout added padding to the bottom of error messages

--- form.php
+++ form.php
@@ -7,7 +7,7 @@ if (isset($errors)) {
     include "includes/errors.php";
 }
 if (isset($item_added)) {
-    echo "<div class='success-container'>";
+    echo "<div class='success-container container'>";
         echo "<div class='item_added'>";
             echo "Item Added!";
         echo "</div>";
