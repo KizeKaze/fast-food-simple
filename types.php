8cd185b117e3ae2dfbc70b88b9b0ca3711698433
kizekaze <rae337@hotmail.com>
2022-06-16 22:29:40 -0500

    added types.php allow admins to add, edit and delete types

---
+++ types.php
@@ -0,0 +1,49 @@
+<?php
+include "includes/header.php";
+include "includes/nav.php";
+
+if(isset($_GET['delete'])) {
+    if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1){
+        $id = $_GET['delete'];
+
+        $params = [
+            'type_id' => $id
+        ];
+        $query = new \App\Classes\Query();
+
+        $result = $query->CustomSQL('SELECT type_id FROM item WHERE type_id = :type_id', $params);
+
+        if (count($result) >= 1) {
+            $errors[] = 'Items in item table depend on type: ID ' . $params['type_id'] . ', ACCESS DENIED';
+        } else {
+            $query->CustomSQL('DELETE FROM type WHERE type_id = :type_id', $params);
+        }
+    }
+}
+
+if(isset($_GET['add'])) {
+    if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1){
+        $type = sanitize($_GET['add']);
+
+        $param = [
+            'type' => $type
+        ];
+        $query = new \App\Classes\Query();
+
+        $query->insert('type', $param);
+    }
+}
+
+if(isset($_POST['type_edit'])) {
+    $query = new \App\Classes\Query();
+    $id = sanitize($_POST['id']);
+    $param = [
+        'type' => sanitize($_POST['type_edit'])
+    ];
+    $query->CustomSQL('UPDATE type SET type = :type WHERE type_id = ' . $id . ' ', $param);
+}
+
+$types = $menu->getTypes();
+
+
+include 'src/forms/types_form.php';
