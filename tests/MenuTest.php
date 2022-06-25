37471daffa9ae943da103a6d6fb9038195423b75
kizekaze <rae337@hotmail.com>
2022-06-25 14:21:22 -0500

    finished Menutest unit testing

---
+++ tests/MenuTest.php
@@ -0,0 +1,113 @@
+<?php
+
+
+use App\Classes\Database;
+use PHPUnit\Framework\TestCase;
+
+class MenuTest extends TestCase
+{
+    public static PDO $db;
+    public static $Menu;
+    public static array $id = [];
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$db = Database::getInstance();
+        self::$Menu = new App\Classes\Menu();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        $id_array = implode(', ', self::$id);
+        $delete_sql = 'DELETE FROM item WHERE id IN(' . '\'' . $id_array . '\')';
+        $delete_stmt = self::$db->prepare($delete_sql);
+        $delete_stmt->execute();
+    }
+
+    public function testGetTypes()
+    {
+        $type = self::$Menu->getTypes();
+
+        $this->assertNotEmpty($type);
+        $this->assertIsArray($type);
+    }
+
+    public function testGetItemsWithSearch()
+    {
+
+        $search = 'Apple';
+        $type = null;
+
+        $params = [
+            'search' => $search,
+            'type' => $type
+        ];
+
+        $item = self::$Menu->getItems($params);
+        $this->assertNotEmpty($item);
+        $this->assertArrayHasKey('name', $item[0]);
+
+        $search = null;
+        $type = 11;
+
+        $params = [
+            'search' => $search,
+            'type' => $type
+        ];
+
+        $item = self::$Menu->getItems($params);
+        $this->assertNotEmpty($item);
+        $this->assertArrayHasKey('type_id', $item[0]);
+    }
+
+    public function testGetItem()
+    {
+
+        $item_id = 206;
+
+        $item = self::$Menu->getItem($item_id);
+        $this->assertNotEmpty($item);
+        $this->assertIsArray($item);
+    }
+
+    public function testUpdateItem()
+    {
+        // add a row into db, use PDO last id or something like that to grab the id of the row we made,
+        // push that ID into our array id property for our teardown and use IN keyword to delete it later!
+
+        $sql = 'SELECT MAX(id) AS item_id FROM item';
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute();
+        $pristine_results =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $name = 'TestLad';
+        $description = 'Sometimes it do what it be';
+        $cost = '1.99';
+        $type_id = '11';
+        $pristine_name = $name;
+
+        $sql = "INSERT INTO item (name, description, cost, type_id) VALUES ('$name', '$description', '$cost', '$type_id')";
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute();
+
+        $inserted_id = self::$db->lastInsertId();
+
+        $this->assertNotEquals($pristine_results, $inserted_id);
+
+        self::$id[] = $inserted_id;
+
+        $name = 'wowie';
+        $description = 'Sometimes';
+        $cost = '17.99';
+        $type_id = '13';
+
+        self::$Menu->updateItem($inserted_id, $name, $description, $cost, $type_id);
+
+        $sql = "SELECT name FROM item WHERE id = $inserted_id";
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute();
+        $filthy_results =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertNotEquals($name, $filthy_results);
+    }
+}
