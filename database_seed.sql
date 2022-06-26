46dba73a8b17f6b4fa882f6bc768a19ffc40f78f
kizekaze <=>
2022-06-25 19:08:20 -0500

    updated database seed to give anyone who wants to test my database some free dummy data

--- database_seed.sql
+++ database_seed.sql
@@ -12,6 +12,21 @@ CREATE TABLE item
         PRIMARY KEY (id)
 );
 
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Carrots', '1 Baby carrots in a bag', 1.99, 2);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Apple', 'An Apple', 0.50, 1);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('WaterMelon', 'A juicy watermelon, only the best', 2.59, 1);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Coke 12 pack', '12 pack of tasty coke', 5.99, 8);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Mt Dew 12 pack', '12 pack of Mt Dew', 5.99, 8);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Eggs', 'a dozen eggs', 0.99, 7);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Chicken Breast', '4 juicy chicken breast', 7.99, 3);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('ground beef', '1 pound of ground beef', 4.99, 4);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Crab legs', 'Crab legs of the sea', 3.99, 5);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Coffee', '942 MG of caffine', 0.69, 9);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Creamer', 'tasty sauce for the coffee', 2.99, 9);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('dr pepper', '23 flavors in one can', 2.99, 8);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('choc milk', 'the richest', 150.34, 6);
+INSERT INTO fast_food.item (name, description, cost, type_id) VALUES ('Baby formula', 'dry powder formula made with real milk', 17.99, 6);
+
 CREATE TABLE type
 (
     type_id int AUTO_INCREMENT,
@@ -19,6 +34,17 @@ CREATE TABLE type
     CONSTRAINT type_pk
         PRIMARY KEY (type_id)
 );
+
+INSERT INTO fast_food.type (type) VALUES ('Fruits');
+INSERT INTO fast_food.type (type) VALUES ('Vegetable');
+INSERT INTO fast_food.type (type) VALUES ('Poulty');
+INSERT INTO fast_food.type (type) VALUES ('Meat');
+INSERT INTO fast_food.type (type) VALUES ('Fish');
+INSERT INTO fast_food.type (type) VALUES ('Dairy');
+INSERT INTO fast_food.type (type) VALUES ('Eggs');
+INSERT INTO fast_food.type (type) VALUES ('Drink');
+INSERT INTO fast_food.type (type) VALUES ('Coffee');
+
 
 INSERT INTO fast_food.type (type)
 VALUES ('Food');
