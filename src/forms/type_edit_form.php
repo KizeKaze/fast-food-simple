b992c9c16af057fdb6151842c7daf25dd267b77d
kizekaze <rae337@hotmail.com>
2022-07-23 16:41:55 -0500

    fixed indenting

--- src/forms/type_edit_form.php
+++ src/forms/type_edit_form.php
@@ -4,15 +4,15 @@
         $type = sanitize($_GET['edit']);
         $id = sanitize($_GET['id']);
     ?>
-            <div class="col-sm-12 col-md-12">
-                <form action="types.php" method="post">
-                    <div class="input-group mb-1">
-                        <span class="input-group-text" id="edit_type">Edit Type</span>
-                        <input type="hidden" name="id" value="<?= $id ?>" >
-                        <input class="form-control" type="text" name="type_edit" value="<?= $type ?>">
-                        <button class="btn btn-success">Edit</button>
-                    </div>
-                </form>
+    <div class="col-sm-12 col-md-12">
+        <form action="types.php" method="post">
+            <div class="input-group mb-1">
+                <span class="input-group-text" id="edit_type">Edit Type</span>
+                <input type="hidden" name="id" value="<?= $id ?>" >
+                <input class="form-control" type="text" name="type_edit" value="<?= $type ?>">
+                <button class="btn btn-success">Edit</button>
             </div>
+        </form>
+    </div>
     <?php endif; ?>
 <?php endif; ?>
