e238be7812aaf77491004bad0521207191bfc713
kizekaze <rae337@hotmail.com>
2022-07-22 19:49:53 -0500

    work in progress

--- vue_groceries.php
+++ vue_groceries.php
@@ -4,14 +4,12 @@ require 'vendor/autoload.php';
 
 
 
-
-
 header('Content-Type: application/json');
 
 $query = new \App\classes\Query();
 
 $lists = $query->CustomSQL('SELECT * FROM item i 
-                    INNER JOIN type t ON i.type_id = t.type_id');
+                                INNER JOIN type t ON i.type_id = t.type_id');
 
 $types = $query->CustomSQL('SELECT type_id, type FROM type');
 
@@ -19,6 +17,7 @@ $array = [
     'list' => $lists,
     'type' => $types
 ];
+
 
 echo json_encode($array);
 
