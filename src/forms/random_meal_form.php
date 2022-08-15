aca2c4c535f0a82bdc5c4710cc0eafd4203c6e81
kizekaze <rae337@hotmail.com>
2022-08-15 18:29:12 -0500

    adjusted line spacing added alternate syntax a  foreach I missed. Changed button to secondary, default didn't look right to me, let me know.

--- src/forms/random_meal_form.php
+++ src/forms/random_meal_form.php
@@ -18,17 +18,17 @@
                 <div class="card-body">
                     <h5 class="card-title">Ingredients You Will Need</h5>
                     <ul>
-                       <?php foreach ($combined as $key => $value) {
-                            echo "<li>$key" . " - " . "$value</li>";
-                       } ?>
+                       <?php foreach ($combined as $key => $value) : ?>
+                            <li><?="$key; " . " - " . "$value"; ?></li>
+                       <?php endforeach; ?>
                     </ul>
                     <h5 class="card-title">Cooking Instructions</h5>
                     <ul>
                         <?php $chunks = explode('.', $meal['strInstructions']); ?>
                         <?php foreach ($chunks as $element) : ?>
                             <?php if ($element && trim(strlen($element)) > 4) : ?>
-                           <li><?= $element ?></li>
-                        <?php endif; ?>
+                                <li><?= $element ?></li>
+                            <?php endif; ?>
                         <?php endforeach; ?>
                     </ul>
                 </div>
@@ -36,10 +36,8 @@
             <div class="card">
                 <h5 class="card-title">Want to follow a YouTube how to?</h5>
                 <div class="card-body">
-                    <div class="btn-group" role="group">
-                        <a class="btn btn-primary" target="@" href="<?= $meal['strYoutube']; ?>"><?= $meal['strMeal']; ?></a>
-                        <a class="btn btn-primary" href="#top">Back To Top</a>
-                    </div>
+                    <a class="btn btn-primary" target="@" href="<?= $meal['strYoutube']; ?>"><?= $meal['strMeal']; ?></a>
+                    <a class="btn btn-secondary" href="#top">Back To Top</a>
                 </div>
             </div>
             <?php endif; ?>
