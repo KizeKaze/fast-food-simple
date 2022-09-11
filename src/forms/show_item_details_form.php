b46dbc84b204aafbcc5a29971829d7831a4e953e
kizekaze <rae337@hotmail.com>
2022-09-11 11:53:12 -0500

    added another <div> to help the orphaned <div> tag. teamwork makes the dream work. corrected indenting

--- src/forms/show_item_details_form.php
+++ src/forms/show_item_details_form.php
@@ -4,21 +4,21 @@
             <form action="" method="get">
                 <img class="mx-auto d-block" src="src/images/<?= $image ?>" width="75%" height="300px" alt="<?= $chunk['name'] ?>">
                 <div class="card-body">
-                <hr>
-                <h5 class="card-title"><?= $chunk['name'] ?></h5>
-                <hr>
-                <p class="card-text"><?= $chunk['description'] ?></p>
-                <hr>
-                <p class="card-text">$<?= $chunk['cost'] ?> each</p>
-                <hr>
-                <?php if ($User->loggedIn()) : ?>
-                <select class="add_qty form-select" aria-label="Quantity select" name="qty">
+                    <hr>
+                    <h5 class="card-title"><?= $chunk['name'] ?></h5>
+                    <hr>
+                    <p class="card-text"><?= $chunk['description'] ?></p>
+                    <hr>
+                    <p class="card-text">$<?= $chunk['cost'] ?> each</p>
+                    <hr>
+                    <?php if ($User->loggedIn()) : ?>
+                        <select class="add_qty form-select" aria-label="Quantity select" name="qty">
                     <?php $menu->showQty(); ?>
-                </select>
-                <div class="card-body">
-                    <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit" name="add">Add Item
-                    </button>
-                    <a class="btn btn-danger card-link" href="/index.php">Go back</a>
+                    </select>
+                    <div class="card-body">
+                        <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit" name="add">Add Item</button>
+                        <a class="btn btn-danger card-link" href="/index.php">Go back</a>
+                    </div>
                 </div>
             </form>
             <?php else : ?>
