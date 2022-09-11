8dc1269cee1faa6c61ec177eb9c2210005281989
kizekaze <rae337@hotmail.com>
2022-09-11 11:42:06 -0500

    removed any ul as I was not using them semantically adjusted indenting

--- src/forms/show_item_details_form.php
+++ src/forms/show_item_details_form.php
@@ -1,37 +1,33 @@
-
 <div class="container">
     <div class="row justify-content-center gx-0">
         <div class="card col-sm-12 col-md-6">
-            <img class="mx-auto d-block" src="src/images/<?= $image ?>" width="75%" height="300px"
-                 alt="<?= $chunk['name'] ?>">
-            <div class="card-body">
+            <form action="" method="get">
+                <img class="mx-auto d-block" src="src/images/<?= $image ?>" width="75%" height="300px" alt="<?= $chunk['name'] ?>">
+                <div class="card-body">
+                <hr>
                 <h5 class="card-title"><?= $chunk['name'] ?></h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item"><?= $chunk['description'] ?></li>
-                <li class="list-group-item">$<?= $chunk['cost'] ?> each</li>
+                <hr>
+                <p class="card-text"><?= $chunk['description'] ?></p>
+                <hr>
+                <p class="card-text">$<?= $chunk['cost'] ?> each</p>
+                <hr>
                 <?php if ($User->loggedIn()) : ?>
-                <form action="" method="get">
-                <select class="add_qty list-group-item form-select form-control" aria-label="Quantity select" name="qty">
+                <select class="add_qty form-select" aria-label="Quantity select" name="qty">
                     <?php $menu->showQty(); ?>
                 </select>
-            </ul>
-            <div class="card-body">
-                <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit" name="add">Add Item</button>
+                <div class="card-body">
+                    <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit" name="add">Add Item
+                    </button>
                     <a class="btn btn-danger card-link" href="/index.php">Go back</a>
-                </form>
-            </div>
+                </div>
+            </form>
             <?php else : ?>
-            </ul>
             <div class="card-body">
                 <a class="btn btn-danger card-link" href="/index.php">Go back</a>
-             </div>
+            </div>
             <?php endif; ?>
         </div>
     </div>
 </div>
 
 <?php include "includes/footer.php" ?>
-
-
-
