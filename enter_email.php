0ef913b47471fd2380bc89ace778f74e5e504455
kizekaze <rae337@hotmail.com>
2022-10-08 13:44:12 -0500

    added failure message for invalid email added logic to check for a valid email

--- enter_email.php
+++ enter_email.php
@@ -2,16 +2,34 @@
 <?php include "includes/nav.php" ?>
 
 <?php
-
- if ($_POST) {
-    echo "Hey, I see you";
- } else {
-     echo "I didnt see your post";
-}
+$query = new \App\Classes\Query();
 
 
+ if (isset($_POST['reset-password'])) {
+    $email = sanitize($_POST['email']);
+
+     if(empty($email)) {
+         $_SESSION['failure'] = "Your email is required";
+         header('Location: enter_email.php');
+         exit();
+     }
+
+     $params = [
+         'email' => $email
+     ];
+
+    $result = $query->CustomSQL("SELECT email FROM users WHERE email = :email", $params);
+
+    if (count($result) <= 0) {
+        $_SESSION['failure'] = "We did not find that email in our system, sorry";
+        header('Location: enter_email.php');
+        exit();
+    }
+
+    echo "if you made it here, you are passed the email validation";
 
 
+ }
 
 
 
