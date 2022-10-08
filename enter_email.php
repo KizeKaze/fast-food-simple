3404b4b2d247dd1d135f5034a2bad352814b2f50
kizekaze <rae337@hotmail.com>
2022-10-08 11:35:39 -0500

    added 'forgot password?' to login form added a form with input checking if I have post WIP

---
+++ enter_email.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/nav.php" ?>
+
+<?php
+
+ if ($_POST) {
+    echo "Hey, I see you";
+ } else {
+     echo "I didnt see your post";
+}
+
+
+
+
+
+
+
+
+
+
+include 'src/forms/enter_email_form.php';
