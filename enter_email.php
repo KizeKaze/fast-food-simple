2a5b00a2fa35975ca8eb4839292d54e2ce54f653
kizekaze <rae337@hotmail.com>
2022-10-13 19:47:09 -0500

    setting a default timezone checking for duplicate email with an unexpired token

--- enter_email.php
+++ enter_email.php
@@ -26,7 +26,12 @@ $query = new \App\Classes\Query();
          exit();
      }
 
-     $duplicate_email = $query->CustomSQL("SELECT email FROM password_resets WHERE email = :email", $params);
+     $params = [
+         'email' => $email,
+         'expired_token' => 0
+     ];
+
+     $duplicate_email = $query->CustomSQL("SELECT email FROM password_resets WHERE email = :email AND expired_token = :expired_token", $params);
 
      if ($duplicate_email) {
         $_SESSION['failure'] = "Email has already been sent, please check your inbox or junk folder";
@@ -36,18 +41,27 @@ $query = new \App\Classes\Query();
 
     $token = bin2hex(random_bytes(50));
 
+     $date = date('m-d-Y');
+     dd($date);
+
+
     $params = [
         'email' => $email,
-        'token' => $token
+        'token' => $token,
+        'timed_expired_token' => date('h:i:sa')
     ];
     $query->insert('password_resets', $params);
 
+     $to = $email;
+     $subject = "Password Reset at rayxproject.com";
 
-    $to = $email;
-    $subject = "Password Reset at rayxproject.com";
-     $msg = "Hi there, click on this <a href=\"http://localhost:9001/new_pass_logic.php?&token=" . $token . ">link</a> to reset your password on our site";
-    $msg = wordwrap($msg,70);
-    $headers = "From: Admin@rayxproject.com";
+     $headers = "From: Admin@rayxproject.com" . "\r\n";
+     $headers .= "MIME-Version: 1.0\r\n";
+     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+     $msg = "Hi there, click on this <a href=\"http://localhost:9001/new_pass_logic.php?token=" . $token . "\">link</a> to reset your password on rayxproject.com";
+     $msg = wordwrap($msg,70);
+
     mail($to, $subject, $msg, $headers);
 
     $argument = [
--- new_pass_logic.php
+++ new_pass_logic.php
@@ -8,6 +8,13 @@ if (isset($_POST['new_password'])) {
 
     $password = sanitize($_POST['password']);
     $confirm_password = sanitize($_POST['confirm_password']);
+    $token = sanitize($_GET['token']);
+
+    $params = [
+        'token' => $token,
+        'expired_token' => 0
+    ];
+    $email = $query->CustomSQL("SELECT email FROM password_resets WHERE token = :token AND expired_token = :expired_token LIMIT 1", $params);
 
     if (empty($password)) {
         $errors[] = "Please enter a password";
@@ -17,15 +24,8 @@ if (isset($_POST['new_password'])) {
         $errors[] = "Passwords did not match";
     }
 
-    $token = sanitize($_GET['token']);
 
-    $params = [
-        'token' => $token
-    ];
-    $email = $query->CustomSQL("SELECT email FROM password_resets WHERE token = :token LIMIT 1", $params);
-
-
-    if ($email[0]['email']) {
+    if (isset($email[0]['email'])) {
         $new_password = password_hash($password, PASSWORD_DEFAULT);
         $params = [
             'password' => $new_password,
@@ -34,13 +34,14 @@ if (isset($_POST['new_password'])) {
         $query->CustomSQL('UPDATE users SET password = :password WHERE email = :email', $params);
 
         $params = [
-            'token' => $token
+            'token' => $token,
+            'expired_token' => 1
         ];
-        $query->CustomSQL('DELETE FROM password_resets WHERE token = :token', $params);
+        $query->CustomSQL('UPDATE password_resets SET expired_token = :expired_token WHERE token = :token', $params);
         $item_added = "Password reset, you can <a href='login.php'>login</a> now";
 
     } else {
-        $errors[] = "Invalid Token, please reach out to the site admin for help";
+        $errors[] = "Expired Token, please click <a href='enter_email.php'>here</a> to try again";
     }
 }
 //assign token here if user has not yet clicked new_password
