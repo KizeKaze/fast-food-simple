bdbde94db521daacbee5b8a70b32e68db0185b74
KizeKaze <rae337@hotmail.com>
2024-03-02 10:55:34 -0600

    cleaned up cron job and removed testing variables

--- cron_job_email.php
+++ cron_job_email.php
@@ -1,22 +1,19 @@
 <?php
 require 'vendor/autoload.php';
+use App\Classes\Database;
 
+$query = new \App\Classes\Query();
 $email_info = new \App\Classes\Cart();
 $email_object = new \App\Classes\Email;
 $pass_object = new \App\Classes\Password();
-use App\Classes\Database;
-
-
 
 $db = Database::getInstance();
 
-$query = new \App\Classes\Query();
 $email_chunks = $query->CustomSQL('SELECT order_id, user_id FROM order_complete WHERE email_sent = 0');
 $password_chunks = $query->CustomSQL('SELECT email, token FROM password_resets WHERE password_sent = 0');
 
 
 if (!$email_chunks && !$password_chunks) {
-    echo "cron job has no tasks to do, shutting down...";
    exit();
 }
 
@@ -40,7 +37,7 @@ if ($email_chunks) {
             ];
             $query->CustomSQL('UPDATE order_complete SET email_sent = 1 WHERE order_id = :order_id', $params);
         } else {
-            echo "Failed to send email";
+            exit();
         }
     }
 }
@@ -53,15 +50,13 @@ if ($password_chunks) {
         $mail_sent = $pass_object->sendPassword($email, $token);
 
         if ($mail_sent) {
-            echo "password reset sent";
-
             $params = [
                 'email' => $chunk['email'],
                 'token' => $chunk['token']
             ];
             $query->CustomSQL('UPDATE password_resets SET password_sent = 1 WHERE email = :email AND token = :token', $params);
         } else {
-            echo "Failed to sent password reset";
+            exit();
         }
     }
 }
