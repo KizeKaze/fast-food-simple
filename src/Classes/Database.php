e370086732239da1a019c8170fb2f3825ecefb1f
KizeKaze <rae337@hotmail.com>
2026-05-11 15:39:02 -0500

    Updated Database for local and production use

--- src/Classes/Database.php
+++ src/Classes/Database.php
@@ -6,8 +6,20 @@ use PDO;
 
 abstract class Database
 {
+
     public static function getInstance(): PDO
     {
-        return new \PDO($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USERNAME'], $_ENV['MYSQL_PASSWORD']);
+        $root = dirname(__DIR__, 2);
+
+        $local = $root . '/.env.local';
+        $prod  = $root . '/.env';
+
+        if (file_exists($local)) {
+            $env = parse_ini_file($local);
+        } else {
+            $env = parse_ini_file($prod);
+        }
+
+        return new PDO($env['MYSQL_HOST'], $env['MYSQL_USERNAME'], $env['MYSQL_PASSWORD']);
     }
 }
