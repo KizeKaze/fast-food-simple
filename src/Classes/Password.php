57de6adabc0fd6f443a4a3d6a2b361287c9fbf24
KizeKaze <rae337@hotmail.com>
2026-05-11 17:04:17 -0500

    Added PHPMailer to Password.php

--- src/Classes/Password.php
+++ src/Classes/Password.php
@@ -2,29 +2,59 @@
 
 namespace App\Classes;
 
-
-use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class Password
 {
+    private function sendSMTP($to, $subject, $html)
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host       = $_ENV['SMTP_HOST'];
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $_ENV['SMTP_USER'];
+            $mail->Password   = $_ENV['SMTP_PASS'];
+            $mail->SMTPSecure = $_ENV['SMTP_SECURE'];
+            $mail->Port       = $_ENV['SMTP_PORT'];
+
+            $mail->setFrom($_ENV['SMTP_USER'], 'Raywebdev');
+            $mail->addAddress($to);
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $html;
+
+            return $mail->send();
+        } catch (Exception $e) {
+            error_log("SMTP Error: {$mail->ErrorInfo}");
+            return false;
+        }
+    }
 
     public function getEmail($params)
     {
-        $query = $query = new \App\Classes\Query();
-
-        return $query->CustomSQL("SELECT email FROM password_resets WHERE token = :token AND expired_token = :expired_token LIMIT 1", $params);
+        $query = new \App\Classes\Query();
+        return $query->CustomSQL(
+            "SELECT email FROM password_resets WHERE token = :token AND expired_token = :expired_token LIMIT 1",
+            $params
+        );
     }
 
     public function isTokenExpired($params)
     {
-        $query = $query = new \App\Classes\Query();
-
-        return $query->CustomSQL("SELECT timed_expired_token FROM password_resets WHERE token = :token", $params);
+        $query = new \App\Classes\Query();
+        return $query->CustomSQL(
+            "SELECT timed_expired_token FROM password_resets WHERE token = :token",
+            $params
+        );
     }
 
     public function updatePassword($password, $email, $token)
     {
-        $query = $query = new \App\Classes\Query();
+        $query = new \App\Classes\Query();
 
         $new_password = password_hash($password, PASSWORD_DEFAULT);
         $params = [
@@ -38,29 +68,21 @@ class Password
             'expired_token' => 1
         ];
         $query->CustomSQL('UPDATE password_resets SET expired_token = :expired_token WHERE token = :token', $params);
-        return "Password reset, you can <a href='login.php'>login</a> now";
 
+        return "Password reset, you can <a href='login.php'>login</a> now";
     }
 
     public function sendPassword($email, $token)
     {
-        $user_email = $email;
-
         $reset_link = "https://www.raywebdev.com/new_pass_logic.php?token=" . $token;
 
-        $msg = "Hi there,<br><br>";
+        $msg  = "Hi there,<br><br>";
         $msg .= "Click on this <a href=\"$reset_link\">link</a> to reset your password on raywebdev.com.<br><br>";
         $msg .= "If you did not request this, you can safely ignore this email.";
 
-        $msg = wordwrap($msg, 70);
-
         $subject = "Password Reset at Raywebdev.com";
 
-        $headers  = "MIME-Version: 1.0\r\n";
-        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-        $headers .= "From: raywebdev@outlook.com\r\n";
-
-        return mail($user_email, $subject, $msg, $headers);
+        return $this->sendSMTP($email, $subject, $msg);
     }
 
     public function pendingEmail()
