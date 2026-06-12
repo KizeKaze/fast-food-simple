b1507ce4c568aa753eea022ab167e52248a0eafc
KizeKaze <rae337@hotmail.com>
2026-06-12 11:27:56 -0500

    chore: refactor index_form.php table structure and fix HTML validity
    - corrected invalid <td> and <tr> placement
    - added missing labels for accessibility
    - fixed form placement inside table cells
    - corrected missing foreach/conditional closing tags
    - added PHPDoc type hints and imports for IDE

--- src/forms/index_form.php
+++ src/forms/index_form.php
@@ -6,21 +6,27 @@
                     <div class="row">
                         <div class="col-sm-12 col-md-6">
                             <div class="input-group mb-1">
-                                <input class="form-control" type="text" name="search" placeholder="Search...">
-                                <input type="submit" name="submit" class="btn btn-primary btn-sm">
+                                <label for="search_text" class="visually-hidden">
+                                    Search by Text
+                                </label>
+                                <input id="search_text" class="form-control" type="text" name="search" placeholder="Search...">
+                                <input type="submit" name="submit" class="btn btn-primary btn-sm" aria-label="Search by text">
                             </div>
                         </div>
                         <div class="col-sm-12 col-md-6">
                             <div class="input-group mb-1">
+                                <label for="search_select" class="visually-hidden">
+                                    Search by Select
+                                </label>
                                 <span class="input-group-text" id="inputGroup-sizing-default">Search By Type</span>
-                                <select name="type" class="form-select form-select">
+                                <select id="search_select" name="type" class="form-select form-select">
                                     <option value="0">All</option>
                                     <?php $results = $menu->getType() ?>
                                     <?php foreach ($results as $row) {
                                         echo "<option value=" . $row['type_id'] . ">" . $row['type'] . "</option>";
                                     } ?>
                                 </select>
-                                <input type="submit" name="submit" class="btn btn-primary btn-sm">
+                                <input type="submit" name="submit" class="btn btn-primary btn-sm" aria-label="Search by type">
                             </div>
                         </div>
                     </div>
@@ -34,82 +40,108 @@
 </div>
 <?php
 if (isset($errors)) {
-    include "includes/errors.php";
+    include __DIR__ ."/../../includes/errors.php";
 } else { ?>
 
 <div class='container' id="main_card">
     <div class="table-responsive">
         <table class="table table-light table-bordered table-hover table-responsive">
             <thead>
-            <?php if ($User->isAdmin()) : ?>
-                <th class="hide_mobile_large">ID</th>
-            <?php endif; ?>
-            <th>Name</th>
-            <th class="hide_mobile_large">Description</th>
-            <th>Cost</th>
-            <th class="hide_mobile_large">Type</th>
-            <?php if ($User->isAdmin()) : ?>
-                <th colspan="2">Options</th>
-                <th>Quantity</th>
-                <th>Cart</th>
-            <?php elseif ($User->loggedIn()) : ?>
-                <th>Quantity</th>
-                <th>Cart</th>
-            <?php endif; ?>
-            </thead>
-            <tbody>
             <tr>
-                <?php } ?>
-                <?php foreach ($result as $row) {
-                $id = $row['id'];
-                $name = $row['name'];
-                $description = $row['description'];
-                $cost = $row['cost'];
-                $type = $row['type'];
-                ?>
                 <?php if ($User->isAdmin()) : ?>
-                    <td class="hide_mobile_large"><?= $id ?></td>
+                    <th class="hide_mobile_large">ID</th>
                 <?php endif; ?>
-                <td><a href="../../show_item_details.php?item=<?= $id ?>" class="text-decoration-none"><?= $name ?></a></td>
-                <td class="hide_mobile_large"><textarea class="form-control" readonly><?=$description ?></textarea></td>
-                <td><?= $cost ?></td>
-                <td class="hide_mobile_large"><?= $type ?></td>
-                <?php if ($User->isAdmin()) : ?>
-                        <td>
-                            <a class="btn btn-primary" href="../../edit_menu_item.php?edit=<?= $id ?>">Edit</a>
-                        </td>
-                    <form action="../../index.php" method="get">
-                        <td>
-                            <button type="submit" class="index_delete btn btn-danger" name="delete" value="<?= $id ?>">Delete</button>
-                        </td>
-                    </form>
-                    <form action="" method="get">
-                        <td>
-                            <select class="add_qty form-select" aria-label="Quantity select" name="qty">
-                                <?php  $menu->showQty(); ?>
-                            </select>
-                        </td>
-                        <td>
 
-                            <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit">Add</button>
-                        </td>
-                    </form>
-                <?php elseif ($User->loggedIn()) :  ?>
-                    <form action="" method="get">
-                        <td>
-                            <select class="add_qty form-select" aria-label="Quantity select" name="qty">
-                                <?php $menu->showQty(); ?>
-                            </select>
-                        </td>
-                        <td>
-                            <button class="index_qty btn btn-primary" value="<?= $id ?>" type="submit">Add</button>
-                        </td>
-                    </form>
+                <th>Name</th>
+                <th class="hide_mobile_large">Description</th>
+                <th>Cost</th>
+                <th class="hide_mobile_large">Type</th>
+
+                <?php if ($User->isAdmin()) : ?>
+                    <th colspan="2">Options</th>
+                    <th>Quantity</th>
+                    <th>Cart</th>
+                <?php elseif ($User->loggedIn()) : ?>
+                    <th>Quantity</th>
+                    <th>Cart</th>
                 <?php endif; ?>
             </tr>
-            <?php } ?>
+            </thead>
+
+            <tbody>
+            <?php foreach ($result as $row) : ?>
+                <tr>
+                    <?php if ($User->isAdmin()) : ?>
+                        <td class="hide_mobile_large"><?= $row['id'] ?></td>
+                    <?php endif; ?>
+
+                    <td>
+                        <a href="../../show_item_details.php?item=<?= $row['id'] ?>" class="text-decoration-none">
+                            <?= $row['name'] ?>
+                        </a>
+                    </td>
+
+                    <td class="hide_mobile_large">
+                        <label for="description<?= $row['id'] ?>" class="visually-hidden">
+                            Description
+                        </label>
+                        <textarea id="description<?= $row['id'] ?>" class="form-control" readonly><?= $row['description'] ?></textarea>
+                    </td>
+
+                    <td><?= $row['cost'] ?></td>
+                    <td class="hide_mobile_large"><?= $row['type'] ?></td>
+
+                    <?php if ($User->isAdmin()) : ?>
+                        <td>
+                            <a class="btn btn-primary" href="../../edit_menu_item.php?edit=<?= $row['id'] ?>">Edit</a>
+                        </td>
+
+                        <td>
+                            <form action="../../index.php" method="get">
+                                <button type="submit" class="index_delete btn btn-danger" name="delete" value="<?= $row['id'] ?>">
+                                    Delete
+                                </button>
+                            </form>
+                        </td>
+
+                        <td>
+                            <form action="" method="get">
+                                <label for="qty_<?= $row['id'] ?>" class="visually-hidden">
+                                    Quantity
+                                </label>
+                                <select id="qty_<?= $row['id'] ?>" class="add_qty form-select" name="qty">
+                                    <?php $menu->showQty(); ?>
+                                </select>
+
+                                <button class="index_qty btn btn-primary" value="<?= $row['id'] ?>" type="submit">
+                                    Add
+                                </button>
+                            </form>
+                        </td>
+
+                    <?php elseif ($User->loggedIn()) : ?>
+
+                        <td>
+                            <form action="" method="get">
+                                <label for="qty_<?= $row['id'] ?>" class="visually-hidden">
+                                    Quantity
+                                </label>
+                                <select id="qty_<?= $row['id'] ?>" class="add_qty form-select" name="qty">
+                                    <?php $menu->showQty(); ?>
+                                </select>
+
+                                <button class="index_qty btn btn-primary" value="<?= $row['id'] ?>" type="submit">
+                                    Add
+                                </button>
+                            </form>
+                        </td>
+
+                    <?php endif; ?>
+                </tr>
+            <?php endforeach; ?>
             </tbody>
         </table>
+        <?php } ?>
     </div>
 </div>
 <script src="/src/js/index.js"></script>
