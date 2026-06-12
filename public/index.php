451e33feb425a1e4bc8ef07befbaed17f6f05e1c
KizeKaze <rae337@hotmail.com>
2026-06-12 18:15:39 -0500

    fix: updated url in error validation

--- public/index.php
+++ public/index.php
@@ -88,7 +88,7 @@ $result = $menu->getItems($params);
 $User = new \App\Classes\User();
 
 if (empty($result)) {
-    $errors[] = "<h4>Hmm... I couldn't find what you were looking for. <a href='/public/index.phpdex.php' class='btn btn-primary'>Go Back</a></h4>";
+    $errors[] = "<h4>Hmm... I couldn't find what you were looking for. <a href='index.php' class='btn btn-primary'>Go Back</a></h4>";
 }
 include __DIR__ . "/../templates/layout/header.php";
 include __DIR__ . "/../templates/layout/nav.php";
