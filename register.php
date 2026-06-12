e4b958156cacca89053b2618ac3053589218ffb8
KizeKaze <rae337@hotmail.com>
2026-06-12 12:00:14 -0500

    chore: adjusted register.php use init.php and set header and nav below logic

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
-<?php include "includes/header.php" ?>
-<?php include "includes/nav.php" ?>
-
 <?php
+
+require_once __DIR__ . '/php-config/init.php';
 
 if($_POST) {
     $username = sanitize($_POST['username']);
@@ -49,5 +48,7 @@ if($_POST) {
         $item_added = "Account created, Click <a href='login.php'>here</a> to login";
     }
 }
-include 'src/forms/register_form.php';
+include __DIR__ . '/includes/header.php';
+include __DIR__ . '/includes/nav.php';
+include __DIR__ . '/src/forms/login_form.php';
 ?>
