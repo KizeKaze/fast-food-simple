4741b2a4e35e9ee2d79997160d3597071d3fb2f6
KizeKaze <rae337@hotmail.com>
2026-06-12 14:30:54 -0500

    chore: disabled error displays

--- php-config/init.php
+++ php-config/init.php
@@ -1,8 +1,8 @@
 <?php
 //enable error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
